3ae6f87141b9063e0362bcfb92dad8007ffdc4f1
OwenReece <algo.firmament@gmail.com>
2016-05-21 18:52:38 +0900

    Update bot.py

--- app/bot.py
+++ app/bot.py
@@ -8,10 +8,10 @@ class Bot(object):
     ans_com = "".join(str(d) for d in temp_list_com)
     temp_list_dat=[ord(t) for t in len(self["data"])]
     ans_dat = "".join(str(d) for d in temp_list_dat)
-    
+
     ans=scientificNotation(ans_com)+scientificNotation(ans_dat)
     return hex(ans)
-    
+
   # Convert the number into scientific notation with 16 digits after "."
   # If power of e is greater than 20, get the number between "." and "e"
   # Else return the number itself
