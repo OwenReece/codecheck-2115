6f60082abd9982cec794b3cc3d26189fbc280cfd
OwenReece <algo.firmament@gmail.com>
2016-05-24 12:18:33 +0900

    Update bot.py

--- app/bot.py
+++ app/bot.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-class Bot(object):
+class Bot:
   #ここに素数判定プログラムを実装してください。
 
-  def generateHash(self):
+  def generateHash(self,command,data,hash):
     temp_list_com=[ord(t) for t in len(self["command"])]
-    ans_com = "".join(str(d) for d in temp_list_com)
+    self.command = "".join(str(d) for d in temp_list_com)
     temp_list_dat=[ord(t) for t in len(self["data"])]
-    ans_dat = "".join(str(d) for d in temp_list_dat)
+    self.data = "".join(str(d) for d in temp_list_dat)
 
-    hash=hex(scientificNotation(ans_com)+scientificNotation(ans_dat))
-    return hash
+    self.hash=hex(scientificNotation(ans_com)+scientificNotation(ans_dat))
+    return self.hash
 
   # Convert the number into scientific notation with 16 digits after "."
   # If power of e is greater than 20, get the number between "." and "e"
@@ -23,7 +23,8 @@ class Bot(object):
     if (int(tmp_res1[1]) > 21):
       tmp_res2 = "".join(str(t) for t in tmp_res1)
       tmp_res3 = tmp_res2.split(".")
-      result = tmp_res3[1]
+      while(tmp_res3[1][0]=="0"):
+        result = tmp_res3[1].lsprit("0")
     else:
       result = num
     return result
