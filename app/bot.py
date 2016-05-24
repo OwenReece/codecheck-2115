eeab9914061fcbc767f60dec8f8f5c27a80249bb
OwenReece <algo.firmament@gmail.com>
2016-05-24 21:29:41 +0900

    Update bot.py

--- app/bot.py
+++ app/bot.py
@@ -6,9 +6,9 @@ class Bot(object):
   def __init__(self,dic={}):
     self.dic = dic
   def generate_hash(self):
-    temp_list_com=[ord(t) for t in len(self["command"])]
+    temp_list_com=[ord(t) for t in len(self.dic["command"])]
     self.command = "".join(str(d) for d in temp_list_com)
-    temp_list_dat=[ord(t) for t in len(self["data"])]
+    temp_list_dat=[ord(t) for t in len(self.dic["data"])]
     self.data = "".join(str(d) for d in temp_list_dat)
 
     self.hash=hex(scientificNotation(ans_com)+scientificNotation(ans_dat))
