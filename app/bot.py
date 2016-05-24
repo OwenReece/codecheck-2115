d2374790259c52addb677f4a600201040c466436
OwenReece <algo.firmament@gmail.com>
2016-05-24 23:06:32 +0900

    Update bot.py

--- app/bot.py
+++ app/bot.py
@@ -1,18 +1,16 @@
-#!/usr/#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from nose.tools import assert_equal
-
 class Bot(object):
   def __init__(self,dic={}):
     self.dic = dic
-    
   def generate_hash(self):
-    temp_list_com=[ord(t) for t in self.dic["command"]]
-    self.command = "".join(str(d) for d in temp_list_com)
-    temp_list_dat=[ord(t) for t in self.dic["data"]]
-    self.data = "".join(str(d) for d in temp_list_dat)
+    self.command=self.dic["command"]
+    self.data=self.dic["data"]
+    temp_list_com=[ord(t) for t in self.command]
+    tmp_com = "".join(str(d) for d in temp_list_com)
+    temp_list_dat=[ord(t) for t in self.data]
+    tmp_data = "".join(str(d) for d in temp_list_dat)
 
-    self.hash=hex(scientificNotation(int(self.data))+scientificNotation(int(self.command)))
+    tmp_hash=hex(int(int(scientificNotation(int(tmp_com)))+int(scientificNotation(int(tmp_data)))))
+    self.hash=tmp_hash.lstrip("0").lstrip("x")
     return self.hash
 
   # Convert the number into scientific notation with 16 digits after "."
@@ -26,8 +24,7 @@ def scientificNotation(num):
     tmp_res2 = "".join(str(t) for t in tmp_res1)
     tmp_res3 = tmp_res2.split(".")
     while(tmp_res3[1][0]=="0"):
-      result = tmp_res3[1].lstrip("0")
-    return result
+      tmp_res3[1] = tmp_res3[1].lstrip("0")
+    return tmp_res3[1]
   else:
-    result = num
-    return result
+    return tmp_res1
